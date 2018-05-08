e0cd38b063aae7fb71ff1eaa053223b3df6a35af
James Ticer <jticer4@icloud.com>
2018-05-08 16:59:48 -0600

    added my release the conway button

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function typo() {
-	let firstParagraph = document.getElementById('first');
+	let firstParagraph = document.getElementById('allText');
 	firstParagraph.style.color = 'red';
 }
 
 function bigLeague() {
-	let bigly = document.getElementById('first');
-	bigly.style.fontSize = '200px';
+	let bigly = document.getElementById('second');
+	bigly.style.fontSize = '150px';
+}
+
+
+function wat()
+{
+	let ohWow = document.getElementById('first');
+	ohWow.innerHTML = ohWow.innerHTML.replace(' ', 'Well actually, ');
 }
