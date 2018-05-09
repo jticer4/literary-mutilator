d8fb425dbbc22e0e634f5280dbb1534124dfb7a4
James Ticer <jticer4@icloud.com>
2018-05-08 20:54:11 -0600

    added css to make the background black and the text yellow in order to better implement the "highlight lies" button. I also changed my bigLeagueText button so that it would make all 3 paragraphs bigger

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 function typo() {
 	let firstParagraph = document.getElementById('allText');
-	firstParagraph.style.color = 'red';
+	firstParagraph.style.color = 'yellow';
+
 }
 
 function bigLeague() {
-	let bigly = document.getElementById('second');
-	bigly.style.fontSize = '150px';
+	let biglyFirst = document.getElementById('first');
+	biglyFirst.style.fontSize = '150px';
+
+	let biglySecond = document.getElementById('second');
+	biglySecond.style.fontSize = '150px';
+
+	let biglyThird = document.getElementById('third');
+	biglyThird.style.fontSize = '150px';
 }
 
 
 function wat()
 {
 	let ohWow = document.getElementById('first');
-	ohWow.innerHTML = ohWow.innerHTML.replace(' ', 'Well actually, ');
+	ohWow.innerHTML = ohWow.innerHTML.replace("It’s about making placeholder text great again", 'Alternative Facts');
 }
